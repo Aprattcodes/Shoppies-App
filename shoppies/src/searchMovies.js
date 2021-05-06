181ea4c4802de0d37f382f79089142d3ff39a2af
Alyssa Pratt <alyssakpratt@gmail.com>
2021-05-06 13:58:06 -0500

    tests api with hard coded search query

--- shoppies/src/searchMovies.js
+++ shoppies/src/searchMovies.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 export default function SearchMovies() {
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    const query = "Titanic";
+
+    const api = `http://www.omdbapi.com/?&apikey=3f094384&s=${query}&type="movie"`;
+
+    const result = await fetch(api);
+    const data = await result.json();
+    console.log(data);
+  }
     return (
-              <form>
+              <form className="form" onSubmit={searchMovies}>
                 <label htmlFor="query">Search for a Movie:</label>
                 <input type="text" className="query"/>
                 <button type="submit" className="button">Search</button>
               </form>
             );
-          }
+}
 
 
