daa1e1e543f38bc2ab7ee0f0b2c43a20eda0ab09
Alyssa Pratt <alyssakpratt@gmail.com>
2021-05-07 13:29:32 -0500

    adds state for input query and movies

--- shoppies/src/searchMovies.js
+++ shoppies/src/searchMovies.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function SearchMovies() {
 
+  //state input query movies
+  const [query, setQuery] = useState('');
+  //state for movies and updates the state
+  const [Title, setMovies] = useState([]);
+
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting");
-
-    const query = "Titanic";
 
     const api = `http://www.omdbapi.com/?&apikey=3f094384&s=${query}&type="movie"`;
 
     const result = await fetch(api);
     const data = await result.json();
-    console.log(data);
+    console.log(data.results);
   }
     return (
               <form className="form" onSubmit={searchMovies}>
                 <label htmlFor="query">Search for a Movie:</label>
-                <input type="text" className="query"/>
+                <input type="text" className="input" name='query'
+                  value={query} onChange={(e) => setQuery(e.target.value)}
+                />
                 <button type="submit" className="button">Search</button>
               </form>
             );
