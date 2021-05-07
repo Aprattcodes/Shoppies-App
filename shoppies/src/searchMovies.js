d4926ad77d8b0e5986d2b674a9d68b7abd190919
Alyssa Pratt <alyssakpratt@gmail.com>
2021-05-07 14:28:56 -0500

    adds relevant items required for search

--- shoppies/src/searchMovies.js
+++ shoppies/src/searchMovies.js
@@ -5,18 +5,23 @@ export default function SearchMovies() {
   //state input query movies
   const [query, setQuery] = useState('');
   //state for movies and updates the state
-  const [Title, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
 
     const api = `http://www.omdbapi.com/?&apikey=3f094384&s=${query}&type="movie"`;
 
+    try {
     const result = await fetch(api);
     const data = await result.json();
-    console.log(data.results);
+    setMovies(data.Search);
+    } catch(err) {
+      console.error(err);
+    }
   }
     return (
+            <> 
               <form className="form" onSubmit={searchMovies}>
                 <label htmlFor="query">Search for a Movie:</label>
                 <input type="text" className="input" name='query'
@@ -24,6 +29,22 @@ export default function SearchMovies() {
                 />
                 <button type="submit" className="button">Search</button>
               </form>
+              <div className="card-list">
+                
+                {movies.filter(movie => movie.Poster).map(movie => ( //filters any movie with no poster
+                    <div className="card" key={movie.imdbID}>
+                    <img className="card--image"
+                       src={movie.Poster}
+                       alt={movie.Title + ' poster'}/>
+                      <div className="card--content">
+                          <h2 className="card--title">{movie.Title}</h2>
+                          <p>Released : {movie.Year}</p>
+                          <button>Nominate</button>
+                      </div>
+                    </div>
+                ))}
+              </div>
+            </>
             );
 }
 
